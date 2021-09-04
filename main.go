6573c6a8df7b1ac3b32ca059d4640ad239e87a1e
Nicholas Duffy <3457341+duffn@users.noreply.github.com>
2021-09-04 17:58:32 -0600

    Add user resource and setup

--- main.go
+++ main.go
@@ -5,27 +5,13 @@ import (
 	"flag"
 	"log"
 
+	"github.com/duffn/terraform-provider-alertlogic/internal/provider"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-	"github.com/hashicorp/terraform-provider-scaffolding/internal/provider"
 )
 
-// Run "go generate" to format example terraform files and generate the docs for the registry/website
-
-// If you do not have terraform installed, you can remove the formatting command, but its suggested to
-// ensure the documentation is formatted properly.
-//go:generate terraform fmt -recursive ./examples/
-
-// Run the docs generation tool, check its repository for more information on how it works and how docs
-// can be customized.
-//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
-
 var (
-	// these will be set by the goreleaser configuration
-	// to appropriate values for the compiled binary
 	version string = "dev"
-
-	// goreleaser can also pass the specific commit if you want
-	// commit  string = ""
 )
 
 func main() {
@@ -37,8 +23,7 @@ func main() {
 	opts := &plugin.ServeOpts{ProviderFunc: provider.New(version)}
 
 	if debugMode {
-		// TODO: update this string with the full name of your provider as used in your configs
-		err := plugin.Debug(context.Background(), "registry.terraform.io/hashicorp/scaffolding", opts)
+		err := plugin.Debug(context.Background(), "registry.terraform.io/hashicorp/duffn/terraform-provider-alertlogic", opts)
 		if err != nil {
 			log.Fatal(err.Error())
 		}
