f33a2bcd09cff283ff90750e7f6479327914e62e
Nicholas Duffy <3457341+duffn@users.noreply.github.com>
2021-09-06 14:28:22 -0600

    Add role_id to example

--- examples/resources/alertlogic_user/resource.tf
+++ examples/resources/alertlogic_user/resource.tf
@@ -3,4 +3,5 @@ resource "alertlogic_user" "user" {
   email        = "bob@bobloblawlaw.com"
   mobile_phone = "555-123-4567"
   active       = true
+  role_ids     = ["F578CCE5-9574-4489-BF05-A04075838DE3"]
 }
